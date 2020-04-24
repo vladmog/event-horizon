57baa189124084311fbf3a2c34d9fd6a78efac25
vladmog <vladmog@gmail.com>
2020-04-24 08:18:46 -0700

    added ability to pull user info from auth0

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
 
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
@@ -10,7 +12,15 @@ const NavBar = () => {
         <button onClick={() => loginWithRedirect({})}>Log in</button>
       )}
 
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+        {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+        {/* NEW - add a link to the home and profile pages */}
+        {isAuthenticated && (
+        <span>
+            <Link to="/">Home</Link>&nbsp;
+            <Link to="/profile">Profile</Link>
+        </span>
+        )}
     </div>
   );
 };
