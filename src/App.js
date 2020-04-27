d9851ef6339d44f23ba28420e93787919fbcae94
vladmog <vladmog@gmail.com>
2020-04-26 19:52:53 -0700

    Added profile read ability

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { useAuth0 } from "./react-auth0-spa";
 
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+
 function App() {
   const { loading } = useAuth0();
 
@@ -11,9 +16,15 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
