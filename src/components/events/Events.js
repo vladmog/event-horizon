9499b55a529bc41c86499444e740aab9b9208c19
vladmog <vladmog@gmail.com>
2020-04-27 15:36:25 -0700

    Sending user email address to authorized BE endpoint

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,37 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators, compose } from "redux";
 import { useAuth0 } from "../../react-auth0-spa";
 
-import {getUser} from "../../redux/actions";
+import { getUser } from "../../redux/actions";
 
-const Events = (props) => {
+const Events = props => {
+	const { user, getTokenSilently } = useAuth0();
 
-    const { user } = useAuth0();
-    console.log(user)
+	useEffect(() => {
+		getTokenSilently().then(token => {
+			props.getUser(token, user.email);
+		});
+	}, []);
 
-  return (
-    <div>
-        <h1>Events</h1>
-        <h2>Hello {user.email}</h2> 
-    </div>
-  );
+	if (!props.isUserRetrieved) {
+		return <div>Getting user info</div>;
+	}
+
+	if (props.isNewUser) {
+		return <h1>Pick a username</h1>;
+	}
+
+	return (
+		<div>
+			<h1>Events</h1>
+			<h2>Hello {user.email}</h2>
+		</div>
+	);
 };
 
 const mapStateToProps = ({ user, events }) => ({
-    isUserRetrieved: user.isUserRetrieved,
-  });
-  
+	isUserRetrieved: user.isUserRetrieved,
+	isNewUser: user.isNewUser,
+});
+
 const mapDispatchToProps = dispatch =>
-bindActionCreators(
-    {
-        getUser
-    },
-    dispatch
-);
+	bindActionCreators(
+		{
+			getUser,
+		},
+		dispatch
+	);
 
 export default compose(
-    withRouter,
-    connect(mapStateToProps, mapDispatchToProps)
+	withRouter,
+	connect(mapStateToProps, mapDispatchToProps)
 )(Events);
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,21 +1,25 @@
-import {
-    GET_USER_SUCCESS,
-} from "../actions";
-  
-  const initialState = {
-      isUserRetrieved: false
-  };
-  
-  export const userReducer = (state = initialState, {type, payload}) => {
-    switch (type) {
+import { GET_USER_SUCCESS } from "../actions";
 
-        case GET_USER_SUCCESS:
-        return {
-            ...state,
-            isUserRetrieved: true,
-        };
-        
-        default:
-            return state;
-    }
-  };
+const initialState = {
+	isUserRetrieved: false,
+};
+
+export const userReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case GET_USER_SUCCESS:
+			let isNewUser;
+			if (payload.length > 0) {
+				isNewUser = false;
+			} else {
+				isNewUser = true;
+			}
+			return {
+				...state,
+				isUserRetrieved: true,
+				isNewUser: isNewUser,
+			};
+
+		default:
+			return state;
+	}
+};
