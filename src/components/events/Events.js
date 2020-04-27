172f3b9d65dae1ba41c0c92b3bee4f3912ad2e87
vladmog <vladmog@gmail.com>
2020-04-27 11:43:18 -0700

    pulling user email address to events page

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -2,15 +2,19 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators, compose } from "redux";
+import { useAuth0 } from "../../react-auth0-spa";
 
 import {getUser} from "../../redux/actions";
 
 const Events = (props) => {
 
+    const { user } = useAuth0();
+    console.log(user)
 
   return (
     <div>
-        <h1>Events</h1>     
+        <h1>Events</h1>
+        <h2>Hello {user.email}</h2> 
     </div>
   );
 };
