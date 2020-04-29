5770c859fa293fa9537e5722fb823a6222d9e4d6
vladmog <vladmog@gmail.com>
2020-04-28 22:06:20 -0700

    Made login redirect uri dynamic

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,47 +4,55 @@ import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators, compose } from "redux";
 
-import {getUser} from "../../redux/actions";
+import { getUser } from "../../redux/actions";
 
-const Landing = (props) => {
-    console.log("isUserRetrieved", props.isUserRetrieved)
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+const Landing = props => {
+	console.log("isUserRetrieved", props.isUserRetrieved);
+	const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+	const currentUrl = window.location.href;
 
-  return (
-    <div>
-        <h1>EVENT HORIZON</h1>
-        <h2>an event planner</h2>
-        <section>
-            <h3>AVAILABILITY</h3>
-            <div>See days where everyone is free at a glance</div>
-        </section>
-        <section>
-            <h3>COST SPLIT</h3>
-            <div>Divide group costs evenly</div>
-        </section>
-        <section>
-            <h3>CHECKLIST</h3>
-            <div>Ensure nothing is forgotten</div>
-        </section>
-        <button onClick={() => loginWithRedirect({redirect_uri: 'http://localhost:3000/events'})}>SIGN IN</button>
-        
-    </div>
-  );
+	return (
+		<div>
+			<h1>EVENT HORIZON</h1>
+			<h2>an event planner</h2>
+			<section>
+				<h3>AVAILABILITY</h3>
+				<div>See days where everyone is free at a glance</div>
+			</section>
+			<section>
+				<h3>COST SPLIT</h3>
+				<div>Divide group costs evenly</div>
+			</section>
+			<section>
+				<h3>CHECKLIST</h3>
+				<div>Ensure nothing is forgotten</div>
+			</section>
+			<button
+				onClick={() =>
+					loginWithRedirect({
+						redirect_uri: `${currentUrl}events`,
+					})
+				}
+			>
+				SIGN IN
+			</button>
+		</div>
+	);
 };
 
 const mapStateToProps = ({ user, events }) => ({
-    isUserRetrieved: user.isUserRetrieved,
-  });
-  
+	isUserRetrieved: user.isUserRetrieved,
+});
+
 const mapDispatchToProps = dispatch =>
-bindActionCreators(
-    {
-        getUser
-    },
-    dispatch
-);
+	bindActionCreators(
+		{
+			getUser,
+		},
+		dispatch
+	);
 
 export default compose(
-    withRouter,
-    connect(mapStateToProps, mapDispatchToProps)
+	withRouter,
+	connect(mapStateToProps, mapDispatchToProps)
 )(Landing);
