cf7fae7ecb871026ea9a5201e1056877d3c82403
vladmog <vladmog@gmail.com>
2020-05-23 17:00:27 -0700

    created join page

--- src/components/invite/Join.js
+++ src/components/invite/Join.js
@@ -5,13 +5,29 @@ import { bindActionCreators, compose } from "redux";
 import { Link } from "react-router-dom";
 
 const Join = props => {
-	let eventHash = props.match.params.eventHash;
-	console.log("eventHash in join: ", eventHash);
+	const joinEvent = () => {
+		const eventHash = props.match.params.eventHash;
+		const userAndHash = {
+			userId: props.userId,
+			eventHash: eventHash,
+		};
+		console.log("userAndHash: ", userAndHash);
+	};
 
-	return <div>JOIN</div>;
+	return (
+		<div>
+			<h1>
+				You are not currently part of this event. Would you like to
+				join?
+			</h1>
+			<button onClick={() => joinEvent()}>YES</button>
+			<button>NO</button>
+		</div>
+	);
 };
 
 const mapStateToProps = ({ user, events }) => ({
+	userId: user.userId,
 	events: events.events,
 	eventHashIndexes: events.eventHashIndexes,
 });
