d295351c09dfcb390484a7b013ffd2a825908907
Vladislav Mogilevskiy <vmogilevskiy@Vladislavs-MacBook-Pro.local>
2020-06-30 15:14:26 -0700

    replaced green with option of colors

--- src/components/calendar/Day.js
+++ src/components/calendar/Day.js
@@ -6,25 +6,37 @@ import { bindActionCreators, compose } from "redux";
 const S = {
 	Container: styled.div`
 		width: 14.2%;
-		height: ${props => props.dayHeight};
+		height: ${(props) => props.dayHeight};
 		border: solid black 1px;
 		box-sizing: border-box;
-		background-color: ${props => props.backgroundColor};
+		background-color: ${(props) => props.backgroundColor};
 	`,
 	Blank: styled.div`
 		width: 14.2%;
-		height: ${props => props.dayHeight};
+		height: ${(props) => props.dayHeight};
 		box-sizing: border-box;
 	`,
 };
 
-const Day = props => {
+const Day = (props) => {
 	const [color, setColor] = useState("white");
+	const colors = {
+		color1: "#FFCE99", // << lightest
+		color2: "#FFB05C",
+		color3: "#FF931F",
+		color4: "#E07400", // << darkest
+	};
+	const colorsObj = {
+		"#FFCE99": true, // << lightest
+		"#FFB05C": true,
+		"#FF931F": true,
+		"#E07400": true, // << darkest
+	};
 	useEffect(() => {
 		if (props.day.date !== "blank" && props.day.availabilitiesCount) {
 			// If on render day is not a blank and has availabilities assigned to it, turn day green
-			setColor("green");
-		} else if (color === "green" && !props.day.availabilitiesCount) {
+			setColor(colors.color1);
+		} else if (color in colorsObj && !props.day.availabilitiesCount) {
 			// If on render day is green and has no availabilities assigned to it, turn day white
 			setColor("white");
 		}
@@ -36,12 +48,13 @@ const Day = props => {
 
 	let dayNumber = props.day.day;
 
-	const handleClick = day => {
+	const handleClick = (day) => {
 		if (props.updateMode) {
+			console.log();
 			let action;
 			if (color === "white") {
 				action = "add";
-			} else if (color === "green") {
+			} else if (color in colorsObj) {
 				action = "remove";
 			}
 			props.handleSelect(day, action);
@@ -64,6 +77,6 @@ const mapStateToProps = ({ user, events, calendar }) => ({
 	updateMode: calendar.updateMode,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch);
 
 export default compose(connect(mapStateToProps, mapDispatchToProps))(Day);
